54e362b1b5fee4a5d5099c1c8cb0e5041079ba30
ashah03 <aditshah00@gmail.com>
2018-01-27 15:27:38 -0800

    Change fetch_data from MQTT to ROS

---
+++ python/frc_utils.py
@@ -0,0 +1,25 @@
+import logging
+
+SERIAL_READER = "serial_reader"
+MOVING_AVERAGE = "moving_average"
+DEVICE = "device"
+AVG_SIZE = "avg_size"
+ENABLED = "enabled"
+COMMAND = "command"
+OOR_VALUES = "oor_values"
+
+OUT_OF_RANGE = "-1".encode("utf-8")
+ROS_OOR = -1
+
+
+ENABLED_VALS = ["ON", "ENABLED", "YES", "1", "TRUE"]
+
+logger = logging.getLogger(__name__)
+
+
+def on_message(mqtt_client, userdata, msg):
+    if msg.topic == userdata[COMMAND]:
+        val = msg.payload.decode('ASCII').upper()
+        is_enabled = val in ENABLED_VALS
+        logger.info("With %s setting %s = %s", val, userdata[COMMAND], is_enabled)
+        userdata[ENABLED] = is_enabled
