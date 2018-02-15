285f75c1f7e23924c6298f236e0b41e3ce308b53
Jacobvs <jacobvs2@gmail.com>
2018-02-14 22:05:36 -0800

    make rospi1 script

---
+++ boot/rospi1.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/short_lidar_left_front.sh
+su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/short_lidar_left_back.sh
