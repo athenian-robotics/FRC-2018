5bafe71cf4a23e117155e11486e7633b153e98dc
Jacobvs <jacobvs2@gmail.com>
2018-02-15 16:26:17 -0800

    add startup scripts for all raspis

--- boot/rospi1.sh
+++ boot/rospi1.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/short_lidar_left_front.sh
-su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/short_lidar_left_back.sh
+su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/lidar_left_front_publisher.sh
+su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/lidar_left_back_publisher.sh
+su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/lidar_right_front_publisher.sh
+su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/lidar_right_back_publisher.sh
---
+++ boot/rospilcd.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/lidar_left_front_publisher.sh
+su - pi -c /home/pi/catkin_ws/src/frc_2018_sensors/bin/lidar_left_back_publisher.sh
